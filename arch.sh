1162821aee03c6a9839583b2b9abfcb174cbf0bb
venky <venky@ground.com>
2021-03-31 18:43:09 +0530

    Scripts after install

---
+++ arch.sh
@@ -0,0 +1,8 @@
+sudo pacman -S nvidia nvidia-utils xorg xorg-xinit bspwm sxkhd dmenu picom nitrogen xfce4-terminal arandr
+mkdir .config
+mkdir .config/bspwm
+mkdir .config/sxkhd
+cp /usr/share/doc/bspwm/examples/bspwmrc .config/bspwm/bspwmrc
+cp /usr/share/doc/bspwm/examples/sxkhdrc .config/sxkhd/sxkhdrc
+
+
---
+++ ubuntu.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt-get upgrade
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+apt-get update
+apt-get install gcc g++ qbittorrent make cmake racket vim
